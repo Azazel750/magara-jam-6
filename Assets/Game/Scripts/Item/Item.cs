8f8f755a8b2dcb98cd3ba7fb0712a73686ccc34a
yusufkryll <yusufemrekarayel@hotmail.com.tr>
2023-11-17 00:24:01 +0300

    sdfs

---
+++ Assets/Game/Scripts/Item/Item.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+
+public class Item : ScriptableObject
+{
+    public string name;
+    public string description;
+}
---
+++ Assets/Game/Scripts/Item/Sword.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Sword", menuName = "Sword", order = 0)]
+public class Sword : Item
+{
+    public int damage;
+}
